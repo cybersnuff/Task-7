52990e229aa0cf6864841e3bb5167acca052714f
Vadim <cybersnuff@yandex.ru>
2024-02-28 23:25:40 +0400

    небольшой графический апдейт для кнопок

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,50 +1,65 @@
 package web.model;
 
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+    @NotBlank(message = "Имя не должно быть пустым")
     @Column(name = "name")
     private String name;
+    @NotBlank(message = "Имя не должно быть пустым")
     @Column(name = "department")
     private String department;
+
     @Column(name = "salary")
     private int salary;
+
     public User() {
     }
+
     public User(String name, String department, int salary) {
         this.name = name;
         this.department = department;
         this.salary = salary;
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public int getSalary() {
         return salary;
     }
+
     public void setSalary(int salary) {
         this.salary = salary;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDepartment() {
         return department;
     }
+
     public void setDepartment(String department) {
         this.department = department;
     }
