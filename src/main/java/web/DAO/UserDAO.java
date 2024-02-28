c011c09cfc1f30be17c7b7ad51a102ad4eb5559f
Vadim <cybersnuff@yandex.ru>
2024-02-28 21:35:24 +0400

    commit

--- src/main/java/web/DAO/UserDAO.java
+++ src/main/java/web/DAO/UserDAO.java
@@ -1,17 +1,11 @@
 package web.DAO;
-
 import web.model.User;
-
 import java.util.List;
 
 public interface UserDAO {
-
     public User getUser(int id);
-
     public List<User> getAllUsers();
-
     public void saveUser(User user);
-
     public void deleteUser(int id);
 
 }
--- src/main/java/web/DAO/UserDaoImpl.java
+++ src/main/java/web/DAO/UserDaoImpl.java
@@ -1,30 +1,19 @@
 package web.DAO;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
-
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDAO {
-
     @PersistenceContext
     private EntityManager entityManager;
-
     @Override
     public User getUser(int id) {
         return entityManager.find(User.class, id);
     }
-
-
     @Override
     public List<User> getAllUsers() {
         return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
@@ -41,13 +30,9 @@ public class UserDaoImpl implements UserDAO {
 
     @Override
     public void deleteUser(int id) {
-
-
         User user = getUser(id);
         if (user != null) {
             entityManager.remove(user);
         }
-
     }
-
 }
